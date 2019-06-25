61896ca96dff24fd6b4da38db1526ee5663ec191
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-25 15:52:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var recipes = {};
 /*1 
 */
 function updateObjectWithKeyAndValue(object, key, value){
-  var newObj = Object.assign({object}, { key: value });
+  var newObj = object.
   return newObj;
 }
 
