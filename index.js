26ac6ed77df7860233bfc3c5bb2157581fb2d671
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-25 15:15:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(Object, key, value){
-  var Object = {key
+  var Object = {key: value
 }
