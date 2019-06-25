5fabce43b9df3a1cc7f455d49e7e3d692dd877fc
Benjamin Ezekiel Goode <ben.e.goode@gmail.com>
2019-06-25 15:14:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value){
   return newRecipes;
 }
 
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  var object
+function destructivelyUpdateObjectWithKeyAndValue(Object, key, value){
+  var Object
 }
